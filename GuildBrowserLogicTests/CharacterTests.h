3f2348b6226dfc512890a3866ad6fe6932b9d204
mariusz radziszewski <radziszewski@iMac-user.local>
2013-02-05 12:15:45 +0100

    + some tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by mariusz radziszewski on 05.02.2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
