eb8c4e8810e7ea2f7fcb2cbe4109d6c3dd7a0c9d
mariusz radziszewski <radziszewski@iMac-user.local>
2013-02-05 11:54:22 +0100

    + logic tests

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by mariusz radziszewski on 05.02.2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
